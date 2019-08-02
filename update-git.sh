a4d057e9a711719a11331b40e6a0549c565a00ee
wthr-pstr <48978987+wthr-pstr@users.noreply.github.com>
2019-08-02 13:14:28 +0100

    Update update-git.sh

--- update-git.sh
+++ update-git.sh
@@ -1,6 +1,6 @@
-cd /root/wthr-pstr
+cd /root/test
 git reset --hard origin/master
 git pull
-chmod +x /root/wthr-pstr/gpiomux.sh
-chmod +x /root/wthr-pstr/update-git.sh
-chmod +x /root/wthr-pstr/nothing.sh
+chmod +x /root/test/gpiomux.sh
+chmod +x /root/test/update-git.sh
+chmod +x /root/test/nothing.sh
